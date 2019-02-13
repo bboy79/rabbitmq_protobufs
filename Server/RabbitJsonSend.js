d22a72323d115d424b63bf1892c73ca78a2cdc45
Ricardo Costeira <rcosteira79@gmail.com>
2019-02-13 01:19:31 +0000

    Updates and renames

---
+++ Server/RabbitJsonSend.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const amqp = require("amqplib/callback_api");
+
+const sendData = (error, channel) => {
+  const q = "json";
+
+  const json = JSON.stringify({
+    id: "56fa595034b950a97a63e3a0",
+    updated_at: Date.now(),
+    created_at: Date.now(),
+    name: "Barry Allen",
+    version: 1,
+    picture: "resources/notANude.jpeg",
+    phones: [
+      {
+        number: "+351987654321",
+        type: 0
+      },
+      {
+        number: "+351123456789",
+        type: 2
+      }
+    ]
+  });
+
+  channel.assertQueue(q, { durable: false });
+  // Note: on Node 6 Buffer.from(msg) should be used
+  channel.sendToQueue(q, new Buffer(json));
+};
+
+amqp.connect("amqp://localhost", (error, connection) => {
+  connection.createChannel(sendData);
+
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
---
+++ Server/RabbitProtobufSend.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const amqp = require("amqplib/callback_api");
+const protobuf = require("protocol-buffers");
+const fs = require("fs");
+
+const userProto = "../rabbitmq_protobufs_android/app/src/main/proto/user.proto";
+
+const sendData = (error, channel) => {
+  const q = "proto";
+  const messages = protobuf(fs.readFileSync(userProto));
+
+  const buffer = messages.User.encode({
+    id: "56fa595034b950a97a63e3a0",
+    updated_at: Date.now(),
+    created_at: Date.now(),
+    name: "Barry Allen",
+    version: 1,
+    picture: "resources/notANude.jpeg",
+    phones: [
+      {
+        number: "+351987654321",
+        type: 0
+      },
+      {
+        number: "+351123456789",
+        type: 2
+      }
+    ]
+  });
+
+  channel.assertQueue(q, { durable: false });
+  channel.sendToQueue(q, buffer);
+};
+
+amqp.connect("amqp://localhost", (error, connection) => {
+  connection.createChannel(sendData);
+
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
